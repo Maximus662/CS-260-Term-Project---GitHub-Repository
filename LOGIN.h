ea0b97ecc6de01a3f41769d889c4a1e899e95eb2
Maximus662 <164969577+Maximus662@users.noreply.github.com>
2024-04-15 13:41:25 -0400

    Update LOGIN.h

--- LOGIN.h
+++ LOGIN.h
@@ -19,8 +19,8 @@ class LogIn : public Account
 private:
 	string userName, password;
 public:
-	LogIn() : Account();
-	LogIn(string _fname, string _lname, string _address, string _email, string _phone, int _ID, double _balance, string _userName, string _password) : Account(_fname, _lname, _address, _email, _phone, _ID, _balance);
+	LogIn() : Account() {};
+	LogIn(string _fname, string _lname, string _address, string _email, string _phone, int _ID, double _balance, string _userName, string _password) : Account(_fname, _lname, _address, _email, _phone, _ID, _balance) {};
 	void registerUser(string _userName, string _password);
 };
 #endif //!LOGIN_H
