b2a1f403e67bf01a6bfffe2bef672c1c9574fcfc
Maximus662 <164969577+Maximus662@users.noreply.github.com>
2024-04-15 13:40:59 -0400

    Update LOGIN.cpp

--- LOGIN.cpp
+++ LOGIN.cpp
@@ -1,19 +1,20 @@
+#include"ACCOUNT.h"
 #include"LOGIN.h"
 
 using namespace std;
 
 
-	LogIn() : Account()
+	LogIn::LogIn() : Account()
 	{
 		userName = "";
 		password = "";
 	}
-	LogIn(string _fname, string _lname, string _address, string _email, string _phone, int _ID, double _balance, string _userName, string _password) : Account(_fname, _lname, _address, _email, _phone, _ID, _balance)
+	LogIn::LogIn(string _fname, string _lname, string _address, string _email, string _phone, int _ID, double _balance, string _userName, string _password) : Account(_fname, _lname, _address, _email, _phone, _ID, _balance)
 	{
 		userName = _userName;
 		password = _password;
 	}
-	void registerUser(string _userName, string _password)
+	void LogIn::registerUser(string _userName, string _password)
 	{
 		string _fname, _lname, _address, _email, _phone;
 		double _balance = 0;
@@ -32,10 +33,8 @@ using namespace std;
 		cin >> _email;
 		cout << "Enter Phone Number: ";
 		cin >> _phone;
-		setCustomer(_fname, _lname, _address, _email, _phone);
+		Account::accountCustomer.setCustomer(_fname, _lname, _address, _email, _phone);
 		setID(_ID);
 		setBalance(_balance);
 		cout << "Your acccount has successfully been created!" << endl;
 	}
-
-};
